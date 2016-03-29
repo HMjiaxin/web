c492aaf81c26651fe10b5754411809a09fcc74b0
zcy <zhangchangyu@hmjiaxin.cn>
2016-03-29 15:54:02 +0800

    更新

---
+++ Harmony/administrator/js/top-upManagement.js
@@ -0,0 +1,147 @@
+/**
+ * Created by a on 2016/3/25.
+ */
+function datatable(){
+    var val=$('#search-input').val();
+    if(val==null){
+        val="";
+    }
+    $('#example').dataTable({
+        "oLanguage": {
+            "sSearch": "搜索",
+            "sLengthMenu": "每页显示 _MENU_ 条记录",
+            "sZeroRecords": "没有检索到数据",
+            "sInfo": "显示 _START_ 至 _END_ 条 &nbsp;&nbsp;共 _TOTAL_ 条",
+            "sInfoFiltered": "(筛选自 _MAX_ 条数据)",
+            "sInfoEmtpy": "没有数据",
+            "sProcessing": "正在加载数据...",
+            "oPaginate":
+            {
+                "sFirst": "首页",
+                "sPrevious": "前一页",
+                "sNext": "后一页",
+                "sLast": "末页"
+            }
+        },
+        "bDestroy":true,
+        "autoWidth": false,
+        "deferRender": true,
+        "ordering": false,
+        "searching": false,
+        "sPaginationType": "full_numbers",
+        "iDisplayLength": 10,//每页显示10条数据
+        "lengthChange": false,
+        "processing": true,
+        "serverSide": true,
+        "ajax": {
+            "url": "http://192.168.0.115:8080/rechargelist",
+            "type": "get", // 这里以什么方式请求服务器，服务器端就以什么方式接收参数
+            "dataType":"jsonp",
+            "data" : {
+                userName : val
+            }
+        },
+        "columns": [
+            { "data": "businessName" },
+            { "data": "contactPerson" },
+            { "data": "score" },
+            { "data": "operation",
+                "render":function(data,opt,aa) {
+                    operation = '<a href="#" data-toggle="modal" data-target="#myModal" onclick=myClick(' + aa.businessId + ',this)>操作</a>'
+                    return operation
+                }
+            }
+        ]
+    });
+}
+datatable();
+$('#search-btn').click(function(){
+    datatable();
+});
+var id=null;
+function myClick(a,e,obj){
+    $('.top-up-input').val('');
+    $('.top-up-prompt').html('');
+    id=a;
+    $('#record').dataTable({
+        "oLanguage": {
+            "sSearch": "搜索",
+            "sLengthMenu": "每页显示 _MENU_ 条记录",
+            "sZeroRecords": "没有检索到数据",
+            "sInfo": "显示 _START_ 至 _END_ 条 &nbsp;&nbsp;共 _TOTAL_ 条",
+            "sInfoFiltered": "(筛选自 _MAX_ 条数据)",
+            "sInfoEmtpy": "没有数据",
+            "sProcessing": "正在加载数据...",
+            "oPaginate":
+            {
+                "sFirst": "首页",
+                "sPrevious": "前一页",
+                "sNext": "后一页",
+                "sLast": "末页"
+            }
+        },
+        "bDestroy":true,
+        "autoWidth": false,
+        "deferRender": true,
+        "ordering": false,
+        "searching": false,
+        "sPaginationType": "full_numbers",
+        "iDisplayLength": 5,//每页显示10条数据
+        "lengthChange": false,
+        "processing": true,
+        "serverSide": true,
+        "ajax": {
+            "url": "http://192.168.0.115:8080/rechargehistorylist",//?draw=1"+Math.random(),
+            "type": "get", // 这里以什么方式请求服务器，服务器端就以什么方式接收参数
+            "dataType":"jsonp",
+            "data" : {
+                businessId : a
+            }
+        },
+        "columns": [
+            { "data": "businessName" },
+            { "data": "score" },
+            { "data": "description" },
+            { "data": "createDate"}
+        ]
+    });
+}
+
+$('.top-up-btn').click(function(){
+    var val=$('.top-up-input').val();
+    var reg=/^[0-9]*[1-9][0-9]*$/;
+    if(reg.exec(val)){
+        $('.top-up-prompt').html(' ');
+        $.ajax({
+            type: 'GET',
+            url: 'http://192.168.0.115:8080/recharge',
+            dataType: 'jsonp',
+            data: {
+                businessId : id,
+                score : val
+            },
+            success : function(data){
+                console.log(data);
+                $('.top-up-prompt').html('充值成功').css({'color':'green'});
+                $('.top-up-input').val('');
+            }
+        });
+    }else{
+        $('.top-up-prompt').html('请输入正确的数字').css({'color':'red'});
+    }
+});
+
+
+
+$('#myModal').on('hidden.bs.modal', function (e) {
+    location.reload(true)
+});
+
+
+
+
+
+
+
+
+
---
+++ Harmony/mediaOwner/js/financialManagement.js
@@ -0,0 +1,137 @@
+/**
+ * Created by a on 2016/3/28.
+ */
+var type=0;
+var businessId=10001;
+function datatable(){
+    $('#record').dataTable({
+        "oLanguage": {
+            "sSearch": "搜索",
+            "sLengthMenu": "每页显示 _MENU_ 条记录",
+            "sZeroRecords": "没有检索到数据",
+            "sInfo": "显示 _START_ 至 _END_ 条 &nbsp;&nbsp;共 _TOTAL_ 条",
+            "sInfoFiltered": "(筛选自 _MAX_ 条数据)",
+            "sInfoEmtpy": "没有数据",
+            "sProcessing": "正在加载数据...",
+            "oPaginate":
+            {
+                "sFirst": "首页",
+                "sPrevious": "前一页",
+                "sNext": "后一页",
+                "sLast": "末页"
+            }
+        },
+        "bDestroy":true,
+        "autoWidth": false,
+        "deferRender": true,
+        "ordering": false,
+        "searching": false,
+        "sPaginationType": "full_numbers",
+        "iDisplayLength": 10,//每页显示10条数据
+        "lengthChange": false,
+        "processing": true,
+        "serverSide": true,
+        "ajax": {
+            "url": "http://192.168.0.115:8080/account/history",
+            "type": "get", // 这里以什么方式请求服务器，服务器端就以什么方式接收参数
+            "dataType":"jsonp",
+            "data" : {
+                businessId : businessId,
+                type : type
+            }
+        },
+        "columns": [
+            { "data": "id" },
+            { "data": "score" },
+            { "data": "description" },
+            { "data": "status",
+                "render":function(data){
+                    if(data==3){
+                        state='<span class="text-danger">拒绝</span>'
+                    }else if(data==0){
+                        state='<span class="text-success">完成</span>'
+                    }else if(data==2){
+                        state='<span>处理中</span>'
+                    }else{
+                        state='<span>申请提交</span>'
+                    }
+                    return state
+                }
+            },
+            { "data": "createdDate"}
+        ]
+    });
+}
+datatable();
+
+$('.record-btn').click(function(e){
+    e=$(this).val();
+    $('.record-btn').removeClass('active');
+    $('this').addClass('active');
+    function record(e){
+        if(e=='所有记录'){
+            return 0
+        }else if(e=='收入记录') {
+            return 2
+        }else if(e=='提现记录') {
+            return 1
+        }
+    }
+    type =record(e);
+    datatable(type)
+});
+var balance=null;
+$('.apply-btn').click(function(){
+    $('.content-apply').show();
+    $('.content-info').hide();
+    $.ajax({
+        type: 'GET',
+        url: 'http://192.168.0.115:8080/getscore',
+        dataType: 'jsonp',
+        data: {
+            businessId : businessId
+        },
+        success: function(data){
+            balance=data;
+            $('#balance').html(data);
+        }
+    });
+});
+
+$('.draw-money-btn').click(function(){
+    var val=$('.ipt-money').val();
+    console.log(balance);
+    console.log(val);
+    var reg=/^[0-9]*[1-9][0-9]*$/;
+    if(reg.exec(val)&&val<=balance){
+        $('.reg-prompt').html(' ');
+        $.ajax({
+            type: 'GET',
+            url: 'http://192.168.0.115:8080/drawcashapply',
+            dataType: 'jsonp',
+            data: {
+                businessId : businessId,
+                score : val
+            },
+            success : function(data){
+                location.reload(true);
+            }
+        });
+    }else if(reg.exec(val)&&val>balance){
+        $('.reg-body').show();
+        $('.reg-prompt').html('提现金额不能大于余额');
+    }else{
+        $('.reg-body').show();
+        $('.reg-prompt').html('请输入正确的数字');
+    }
+});
+
+
+
+
+
+
+
+
+
+
