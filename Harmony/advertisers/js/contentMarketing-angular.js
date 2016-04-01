7d60af03e8f21d71484c5965af4a7976adbf37bc
zcy <zhangchangyu@hmjiaxin.cn>
2016-04-01 16:13:05 +0800

    替换

--- Harmony/advertisers/js/contentMarketing-angular.js
+++ Harmony/advertisers/js/contentMarketing-angular.js
@@ -4,7 +4,6 @@
 var MarketingApp = angular.module('MarketingApp', []);
 
 MarketingApp.controller('generalize',function($scope){
-    $scope.chosen='无';
     /*$scope.startDate='2016-03-01';
     $scope.endDate='2016-03-02';*/
     $scope.budget='0';
--- Harmony/advertisers/js/contentMarketing.js
+++ Harmony/advertisers/js/contentMarketing.js
@@ -1,6 +1,7 @@
 /**
  * Created by a on 2016/2/24.
  */
+var businessId=10013;
 /*折线图*/
 var lineChart1,pieTimer;
 $(function () {
@@ -161,15 +162,101 @@ $(function () {
         "serverSide": true,*/
     });
 });
+/*下发消息库鼠标悬停显示标题*/
+function showTitle(){
+    $('.material-wrapper>ul').find('li').mousemove(function(){
+        $(this).find('.rich-mask').show();
+        $(this).find('.rich-title').show();
+    }).mouseout(function(){
+        $(this).find('.rich-mask').hide();
+        $(this).find('.rich-title').hide();
+    }).click(function(){
+        $('.send-down-sample').hide('slow');
+        $('.send-down-preview-b').show('slow');
+        $('.send-down-preview-s').show('slow');
+        var src=$(this).find('img')[0].src;
+        $('.send-down-preview-b img').attr("src",src);
+        $('.send-down-preview-s img').attr("src",src);
+        var title=$(this).find('.rich-title')[0].innerHTML;
+        $('.send-down-preview-b .send-down-msg-title').html(title);
+        $('.send-down-preview-s p').html(title);
+        $('.preview-title').html(title);
+        var author=$(this).attr('author');
+        $('.preview-author').html(author);
+        var digest=$(this).attr('digest');
+        $('.preview-body').html(digest);
+        var sourceurl=$(this).attr('sourceurl');
+        $('.preview-link').html(sourceurl);
+});
+}
+/*下发消息删除*/
+function delMsg(){
+    $('.wechat-details-h li a').find('.fa-trash-o').click(function(){
+        $('.send-down-sample').show('slow');
+        $('.send-down-preview-b').hide('slow');
+        $('.send-down-preview-s').hide('slow');
+        $('.preview-title').html('下发示例');
+        $('.preview-author').html('Parllay');
+        $('.preview-body').html('下发示例,请点击图文进行选择下发示例');
+        $('.preview-link').html('http://www.parllay.cn/');
+    });
+}
+function library(q,w,e){
+    $('#news-library').empty();
+    $.ajax({
+        type: 'post',
+        url: 'http://192.168.0.115:8080/postlibrary',
+        dataType: 'jsonp',
+        data: {
+            businessId : q,
+            number : w,
+            tag: e
+        },
+        success : function(data){
+            for(var i= 0; i<data.length; i++){
+                $('#news-library').append('<li author="'+data[i].author+'" digest="'+data[i].digest+'" sourceUrl="'+data[i].sourceUrl+'">'
+                    +'<img src='+data[i].imageUrl+'>'
+                    +'<div class="rich-mask"></div>'
+                    +'<div class="rich-title">'+data[i].title+'</div>'
+                    +'</li>'
+                );
+            }
+            showTitle();
+            delMsg()
+        },
+        error: function(){
+            console.log('异步请求发生错误！');
+            console.log(arguments);
+        }
+    });
+}
 /*新增推广内容*/
 $('.add-pro').click(function(){
     $('.content-pro').hide();
     $('.content-add-pro').show();
+    library(businessId,7,"");
+    chooseType();
+});
+/*按标签搜索*/
+$('#search-tab').click(function(){
+    var tag=null;
+    tag=$(this).parent().find('p').html();
+    if(tag=='按标签搜索'){
+        tag=''
+    }
+    console.log(tag);
+    library(businessId,7,tag)
 });
 /*7、30、全部--切换*/
 $('.body-content-title>.btn-group').find('button').click(function(){
     $(".body-content-title>.btn-group").find("button").removeClass('active');
     $(this).addClass('active');
+    var num=$(this).val();
+    var tag=$('#search-tab').parent().find('p').html();
+    if(tag=='按标签搜索'){
+        tag=''
+    }
+    library(businessId,num,tag)
 });
 /*最近7天、最近30天、全部--切换*/
 $('.pro-title>.btn-group').find('button').click(function(){
@@ -192,20 +279,97 @@ $(function(){
         $(".search-pop").hide();
     });
 });
-/*下发示例左右框高度相同*/
+/*/!*下发示例左右框高度相同*!/
 var h=$('.wechat-details-s').height();
-$('.wechat-details-body-right').height(h);
-/*选择插件*/
-var config = {
-    '.chosen-select'           : {},
-    '.chosen-select-deselect'  : {allow_single_deselect:true},
-    '.chosen-select-no-single' : {disable_search_threshold:10},
-    '.chosen-select-no-results': {no_results_text:'Oops, nothing found!'},
-    '.chosen-select-width'     : {width:"95%"}
-};
-for (var selector in config) {
-    $(selector).chosen(config[selector]);
+$('.wechat-details-body-right').height(h);*/
+function chooseType(){
+    $.ajax({
+        type: 'post',
+        url: 'http://192.168.0.115:8080/wechattype',
+        dataType: 'jsonp',
+        success : function(data){
+            console.log(data);
+            for(var i= 0; i<data.length; i++){
+                $('.chosen-select').append(
+                    '<option value="'+data[i].id+'" name="'+data[i].className +'">'+data[i].className+'&nbsp;&nbsp;</option>'
+                )
+
+            }
+            $(".chosen-select").trigger("liszt:updated");
+        },
+        error: function(){
+            console.log('异步请求发生错误！');
+            console.log(arguments);
+        }
+    });
 }
+/*选择插件*/
+$(function(){
+    $('.chosen-select').chosen();
+});
+/*流量主类别选择*/
+$('.chosen-select').change(function(){
+    var val=$(this).val().toString();
+    console.log(val);
+    var text=$('.result-selected').text();
+    if(text==''){
+        $('#fetch-detail').text('无');
+    }else{
+        $('#fetch-detail').text(text);
+    }
+    showmMedia(val);
+});
+function showmMedia(r){
+    $.ajax({
+        type: 'post',
+        url: 'http://192.168.0.115:8080/getmedia',
+        dataType: 'jsonp',
+        data:{
+            types : r
+        },
+        success : function(data){
+            console.log(data);
+            for(var i= 0; i<data.length; i++){
+                $('.media-representatives-content').append(
+                    '<ul class="pull-left media-representatives-body">'
+                    +'<li class="pull-left media-representatives-img">'
+                    +'<img src="'+data[i].accountImageUrl+'"/>'
+                    +'<div class="qrcode">'
+                    +'<img src="'+data[i].QRCode+'"/>'
+                    +'</div>'
+                    +'</li>'
+                    +'<li class="pull-left media-representatives-text">'
+                    +'<p>'+data[i].name+'</p>'
+                    +'<p class="fs">粉丝数:'+data[i].fanQuantity+'</p>'
+                    +'</li>'
+                    +'<li class="pull-right media-representatives-close">'
+                    +'<i class="fa fa-close media-close"></i>'
+                    +'<input type="checkbox" class="media-choose"/>'
+                    +'</li>'
+                    +'<li class="pull-right media-representatives-qrcode">'
+                    +'<i class="fa fa-qrcode media-qrcode"></i>'
+                    +'</li>'
+                    +'</ul>'
+                )
+
+            }
+        },
+        error: function(){
+            console.log('异步请求发生错误！');
+            console.log(arguments);
+        }
+    });
+}
+
+
+
+
+
+
+
+
+
+
 /*出价模式*/
 $(function(){
     $(".model-wrapper p").click(function(){
@@ -255,8 +419,6 @@ $('.media-qrcode').mousemove(function(){
 }).mouseout(function(){
     $(this).parent().parent().find($('.qrcode')).hide();
 });
-
-
 
 
 
